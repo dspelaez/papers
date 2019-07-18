98f0c1966bea120e621339b9ce7da97ece1d6ec5
Daniel Santiago <dspelaez@gmail.com>
2019-07-18 16:50:14 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Daniel Santiago <dpelaez@cicese.edu.mx>
+#
+# Distributed under terms of the GNU/GPL license.
+
+from setuptools import setup
+
+setup(
+    name='papers',
+    version='0.1',
+    py_modules=['papers'],
+    install_requires=[
+        'bibtexparser',
+        'requests',
+        'Click',
+        'pyyaml',
+        'inquirer',
+        'colorama',
+        'scihub'
+    ],
+    entry_points='''
+        [console_scripts]
+        papers=papers:cli
+    ''',
+)
